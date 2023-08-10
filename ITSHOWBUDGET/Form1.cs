4609bee8a395dec8e14de165d4d1e6876896c3e3
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-08-10 13:37:54 +0900

    GUI 완성

--- ITSHOWBUDGET/Form1.cs
+++ ITSHOWBUDGET/Form1.cs
@@ -16,5 +16,10 @@ namespace ITSHOWBUDGET
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
